4576e18ee607d4f83237520e93f85d959159db60
DevFabrizio <rockout94@hotmail.it>
2023-06-07 08:13:45 +0000

    built view and urls for post

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-fab.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog-fab.herokuapp.com', 'localhost', '8000-devfabrizio-djangoblog-dcpuqvlel0b.ws-eu98.gitpod.io']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
