425b1ffa91336184f516d0e81813125a1a930e71
DevFabrizio <rockout94@hotmail.it>
2023-06-08 07:20:56 +0000

    add sign up functionality

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-fab.herokuapp.com', 'localhost', '8000-devfabrizio-djangoblog-dcpuqvlel0b.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['django-blog-fab.herokuapp.com', 'localhost', '8000-devfabrizio-djangoblog-dcpuqvlel0b.ws-eu99.gitpod.io']
 
 
 # Application definition
@@ -40,12 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
